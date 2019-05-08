20c4387df5737aff61cc80c04f43cc8f40fb79df
Dovry <beepboopvm@gmail.com>
2019-05-08 07:52:59 +0200

    Update IP-2-DISCORD.sh

--- IP-2-DISCORD.sh
+++ IP-2-DISCORD.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+# 172 addresses are Docker addresses too, so you might want to change the last
+# "\d+" to \24, or whatever your subnet mask is.
 IP="$(ip a | grep -Po "(10|172|192)\.(\d+\.)+\d+\/\d+")"
 WEBHOOK='<DISCORD_WEBHOOK_URL>'
 USERNAME='IP-bot'
