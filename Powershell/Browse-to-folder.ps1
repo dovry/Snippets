05e899717fbd3578d02bb1eb4b9a492c81e053da
Dovry <beepboopvm@gmail.com>
2019-05-30 19:05:51 +0200

    Update Browse-to-folder.ps1

--- Powershell/Browse-to-folder.ps1
+++ Powershell/Browse-to-folder.ps1
@@ -1,3 +1,7 @@
 $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+
+# Opens a GUI to select a folder
 [void]$FolderBrowser.ShowDialog()
+
+# Assign the selected folder to a variable
 $FolderBrowser.SelectedPath
